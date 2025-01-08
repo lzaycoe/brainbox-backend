a4591b23cd117a770e13dccf078c63eba530073f
Minh Vương <hardingadonis@gmail.com>
2025-01-08 14:56:20 +0700

    add: add module aliasing and prettier plugin for import sorting

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+
+import { AppModule } from '@/app.module';
+
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+require('module-alias/register');
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
