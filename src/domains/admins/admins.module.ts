121f00d9a4209afe32532e571f325374afb9a725
Minh Vương <hardingadonis@gmail.com>
2025-01-21 16:20:13 +0700

    refactor: reorganize PrismaService and update imports

--- src/domains/admins/admins.module.ts
+++ src/domains/admins/admins.module.ts
@@ -22,7 +22,7 @@ import { Module } from '@nestjs/common';
 
 import { AdminsController } from '@/admins/admins.controller';
 import { AdminsService } from '@/admins/admins.service';
-import { PrismaService } from '@/prisma.service';
+import { PrismaService } from '@/providers/prisma.service';
 
 @Module({
 	controllers: [AdminsController],
