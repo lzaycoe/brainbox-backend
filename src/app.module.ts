3d13e32e3df387a0a61a9c7fc076b6602a483efa
Minh Vương <hardingadonis@gmail.com>
2025-01-21 08:29:25 +0700

    add: integrate AuthModule and update findByUsername method in AdminsService

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,11 +21,16 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
+import { AuthModule } from '@/auth/auth.module';
 import { DomainsModule } from '@/domains/domains.module';
 import { MorganMiddleware } from '@/middlewares/morgan.middleware';
 
 @Module({
-	imports: [ConfigModule.forRoot({ isGlobal: true }), DomainsModule],
+	imports: [
+		ConfigModule.forRoot({ isGlobal: true }),
+		AuthModule,
+		DomainsModule,
+	],
 	controllers: [],
 	providers: [],
 })
