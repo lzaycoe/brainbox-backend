e0ad0b192396a1f9ac4d86554df77c42a56fba33
Quốc Chương <quocchuong2609@gmail.com>
2025-02-14 19:36:40 +0700

    add: create DTO for course creation with validation rules

---
+++ src/domains/courses/dto/create-course.dto.ts
@@ -0,0 +1,33 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsIn, IsNotEmpty } from 'class-validator';
+
+export class CreateCourseDto {
+	@ApiProperty()
+	@IsNotEmpty()
+	title: string;
+
+	@ApiProperty()
+	description: string;
+
+	@ApiProperty()
+	@IsNotEmpty()
+	price: number;
+
+	@ApiProperty()
+	thumbnail: string;
+
+	@ApiProperty()
+	@IsNotEmpty()
+	tag: string;
+
+	@ApiProperty()
+	@IsIn(['pending', 'approved', 'rejected'])
+	status: string;
+
+	@ApiProperty()
+	@IsNotEmpty()
+	teacherId: number;
+
+	@ApiProperty()
+	public: boolean;
+}
