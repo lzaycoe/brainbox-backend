cc595f90d569249069e83d357a63bea5cc80f8bd
Quốc Chương <quocchuong2609@gmail.com>
2025-02-14 20:57:00 +0700

    add: implement create course functionality in CoursesController and CoursesService with logging

--- src/domains/courses/courses.controller.ts
+++ src/domains/courses/courses.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
+import { CoursesService } from '@/courses/courses.service';
+import { CreateCourseDto } from '@/courses/dto/create-course.dto';
+
+@ApiTags('Courses')
 @Controller('courses')
-export class CoursesController {}
+export class CoursesController {
+	constructor(private readonly coursesService: CoursesService) {}
+
+	@Post()
+	async create(@Body() dto: CreateCourseDto) {
+		return await this.coursesService.create(dto);
+	}
+}
--- src/domains/courses/courses.service.ts
+++ src/domains/courses/courses.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common';
 
+import { CreateCourseDto } from '@/courses/dto/create-course.dto';
 import { PrismaService } from '@/providers/prisma.service';
 
 @Injectable()
@@ -7,4 +8,21 @@ export class CoursesService {
 	private readonly logger = new Logger(CoursesService.name);
 
 	constructor(private readonly prismaService: PrismaService) {}
+
+	async create(dto: CreateCourseDto): Promise<any> {
+		try {
+			const newCourse = await this.prismaService.course.create({
+				data: { ...dto },
+			});
+
+			this.logger.log(`Course with id '${newCourse.id}' created`);
+			this.logger.debug('Course', newCourse);
+
+			return newCourse;
+		} catch (error: any) {
+			this.logger.error(error);
+
+			throw error;
+		}
+	}
 }
