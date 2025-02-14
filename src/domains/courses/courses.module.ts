39622230f2d333417975df1e8826ad244dbce465
Quốc Chương <quocchuong2609@gmail.com>
2025-02-14 19:25:26 +0700

    update: add PrismaService to CoursesModule providers

--- src/domains/courses/courses.module.ts
+++ src/domains/courses/courses.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { CoursesController } from '@/courses/courses.controller';
 import { CoursesService } from '@/courses/courses.service';
+import { PrismaService } from '@/providers/prisma.service';
 
 @Module({
 	controllers: [CoursesController],
-	providers: [CoursesService],
+	providers: [CoursesService, PrismaService],
 	exports: [CoursesService],
 })
 export class CoursesModule {}
