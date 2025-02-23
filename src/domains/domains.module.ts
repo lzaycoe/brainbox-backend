e34633286f0ed66ee0b45ddf3b2ef15cc0d2378f
Quốc Chương <quocchuong2609@gmail.com>
2025-02-23 11:49:53 +0700

    update: add PaymentsModule to DomainsModule and update path mappings in tsconfig

--- src/domains/domains.module.ts
+++ src/domains/domains.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 
 import { AdminsModule } from '@/admins/admins.module';
 import { CoursesModule } from '@/courses/courses.module';
+import { PaymentsModule } from '@/payments/payments.module';
 
 @Module({
-	imports: [AdminsModule, CoursesModule],
+	imports: [AdminsModule, CoursesModule, PaymentsModule],
 })
 export class DomainsModule {}
