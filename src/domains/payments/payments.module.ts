7074e10987ad4fda2a773858fdec55af1664ec1b
Minh Vương <hardingadonis@gmail.com>
2025-02-24 13:03:49 +0700

    update: refactor PaymentsService to utilize the new structure

--- src/domains/payments/payments.module.ts
+++ src/domains/payments/payments.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
+import payOSConfig from '@/configs/payos.config';
 import { CoursesService } from '@/courses/courses.service';
 import { OrdersService } from '@/orders/orders.service';
 import { PaymentsController } from '@/payments/payments.controller';
@@ -7,6 +9,7 @@ import { PaymentsService } from '@/payments/payments.service';
 import { PrismaService } from '@/providers/prisma.service';
 
 @Module({
+	imports: [ConfigModule.forFeature(payOSConfig)],
 	controllers: [PaymentsController],
 	providers: [PaymentsService, PrismaService, OrdersService, CoursesService],
 	exports: [PaymentsService],
