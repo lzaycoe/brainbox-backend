c93964428db63313df9947fb3d19b48f146434fb
Quốc Chương <quocchuong2609@gmail.com>
2025-03-07 10:33:30 +0700

    add: include Revenues module in DomainsModule and update tsconfig paths

--- src/domains/domains.module.ts
+++ src/domains/domains.module.ts
@@ -4,15 +4,17 @@ import { AdminsModule } from '@/admins/admins.module';
 import { ChatsModule } from '@/chats/chats.module';
 import { CoursesModule } from '@/courses/courses.module';
 import { PaymentsModule } from '@/payments/payments.module';
+import { RevenuesModule } from '@/revenues/revenues.module';
 import { UsersModule } from '@/users/users.module';
 
 @Module({
 	imports: [
 		AdminsModule,
+		ChatsModule,
 		CoursesModule,
 		PaymentsModule,
+		RevenuesModule,
 		UsersModule,
-		ChatsModule,
 	],
 })
 export class DomainsModule {}
