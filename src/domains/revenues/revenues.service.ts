b36ff3121d185b35c5505b6a192ea0f5276efb32
Quốc Chương <quocchuong2609@gmail.com>
2025-03-09 21:38:08 +0700

    update: streamline RevenuesController and RevenuesService with improved methods and logging

--- src/domains/revenues/revenues.service.ts
+++ src/domains/revenues/revenues.service.ts
@@ -1,27 +1,112 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 
+import { PrismaService } from '@/providers/prisma.service';
 import { CreateRevenueDto } from '@/revenues/dto/create-revenue.dto';
-import { UpdateRevenueDto } from '@/revenues/dto/update-revenue.dto';
+
+// import { UpdateRevenueDto } from '@/revenues/dto/update-revenue.dto';
 
 @Injectable()
 export class RevenuesService {
-	create(createRevenueDto: CreateRevenueDto) {
-		return 'This action adds a new revenue' + createRevenueDto;
+	private readonly logger = new Logger(RevenuesService.name);
+
+	constructor(private readonly prismaService: PrismaService) {}
+
+	async create(createRevenueDto: CreateRevenueDto) {
+		const existingRevenue = await this.prismaService.revenue.findFirst({
+			where: {
+				teacherId: createRevenueDto.teacherId,
+				courseId: createRevenueDto.courseId,
+			},
+		});
+
+		if (existingRevenue) {
+			this.logger.debug(
+				`Revenue for teacher ${createRevenueDto.teacherId} and course ${createRevenueDto.courseId} already exists`,
+			);
+
+			return existingRevenue;
+		}
+
+		const newRevenue = await this.prismaService.revenue.create({
+			data: createRevenueDto,
+		});
+
+		this.logger.debug(
+			`Revenue for teacher ${createRevenueDto.teacherId} and course ${createRevenueDto.courseId} created successfully`,
+		);
+
+		this.logger.log('Revenue created successfully');
+
+		return newRevenue;
 	}
 
-	findAll() {
-		return `This action returns all revenues`;
+	async findAllByTeacher(teacherId: number) {
+		const revenue = await this.prismaService.revenue.findMany({
+			where: { teacherId },
+		});
+
+		if (!revenue) {
+			this.logger.error(`Revenue for teacher ${teacherId} not found`);
+
+			throw new NotFoundException(`Revenue for teacher ${teacherId} not found`);
+		}
+
+		return revenue;
 	}
 
-	findOne(id: number) {
-		return `This action returns a #${id} revenue`;
+	async FindOneByTeacherAndCourse(teacherId: number, courseId: number) {
+		const revenue = await this.prismaService.revenue.findFirst({
+			where: { teacherId, courseId },
+		});
+
+		if (!revenue) {
+			this.logger.error(
+				`Revenue for teacher ${teacherId} and course ${courseId} not found`,
+			);
+
+			throw new NotFoundException(
+				`Revenue for teacher ${teacherId} and course ${courseId} not found`,
+			);
+		}
+
+		return revenue;
 	}
 
-	update(id: number, updateRevenueDto: UpdateRevenueDto) {
-		return `This action updates a #${id} revenue` + updateRevenueDto;
-	}
+	async calculateRevenue(teacherId: number, courseId: number, price: number) {
+		const revenue = await this.prismaService.revenue.findFirst({
+			where: { teacherId, courseId },
+		});
 
-	remove(id: number) {
-		return `This action removes a #${id} revenue`;
+		if (!revenue) {
+			this.logger.error(
+				`Revenue for teacher ${teacherId} and course ${courseId} not found`,
+			);
+
+			throw new NotFoundException(
+				`Revenue for teacher ${teacherId} and course ${courseId} not found`,
+			);
+		}
+
+		const totalRevenue = +revenue.totalRevenue + price;
+		const totalWithdrawn = revenue.totalWithdrawn;
+		const serviceFee = totalRevenue * 0.1;
+		const netRevenue = totalRevenue - serviceFee;
+		const availableForWithdraw = netRevenue - +totalWithdrawn;
+
+		const updatedRevenue = await this.prismaService.revenue.update({
+			where: { id: revenue.id },
+			data: {
+				totalRevenue,
+				serviceFee,
+				netRevenue,
+				availableForWithdraw,
+			},
+		});
+
+		this.logger.debug(
+			`Revenue for teacher ${teacherId} and course ${courseId} updated successfully`,
+		);
+
+		return updatedRevenue;
 	}
 }
